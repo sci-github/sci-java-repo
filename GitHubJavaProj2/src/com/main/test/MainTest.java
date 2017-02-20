d8dc0a49913a349c7b140f459ad28f7fe20c694b
u146953 <u146953@DTCJP451268R.ent.wfb.bank.corp>
2017-02-20 16:48:40 -0500

    my first project push to Github Public Repo

---
+++ GitHubJavaProj2/src/com/main/test/MainTest.java
@@ -0,0 +1,11 @@
+package com.main.test;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main Test!!!!");
+
+	}
+
+}
---
+++ GitHubJavaProj2/src/com/main/test/SecondMainTest.java
@@ -0,0 +1,10 @@
+package com.main.test;
+
+public class SecondMainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
